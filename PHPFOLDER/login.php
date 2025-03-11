1d57ddf442c628a81f01b6a6819fdfaff9f88317
portfolio <geletamamo0@gmail.com>
2025-03-11 11:07:44 +0300

    change

--- PHPFOLDER/login.php
+++ PHPFOLDER/login.php
@@ -52,33 +52,7 @@ if (isset($_POST['login'])) {
 
 <script>  
     
-    ///Ajax
-    
-$(document).ready(function() {
-    $('#loginForm').on('submit', function(event) {
-        event.preventDefault(); // Prevent the form from submitting normally
-
-        var fname = $('#fname').val();
-        var password = $('#password').val();
-
-        $.ajax({
-            url: 'login.php',
-            type: 'POST',
-            data: {
-                fname: fname,
-                password: password,
-                login: true
-            },
-            success: function(response) {
-                if (response === "success") {
-                    window.location.href = 'home.php'; // Redirect on success
-                } else {
-                    $('#error-message').text(response); // Display error message
-                }
-            }
-        });
-    });
-});
+   
 </script>
 </body>
 </html></div>
